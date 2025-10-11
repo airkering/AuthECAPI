afb4784244e8453a6b36f65409595bd4faddc744
Yavuz Beydogan <y.beydogan33@gmail.com>
2025-10-11 20:56:46 +0300

    Initial Commit

---
+++ Extensions/AppConfigExtensions.cs
@@ -0,0 +1,30 @@
+﻿using AuthECAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace AuthECAPI.Extensions
+{
+    public static class AppConfigExtensions
+    {
+        public static WebApplication ConfigureCORS(this WebApplication app,
+            IConfiguration config)
+        {
+            // Configure the HTTP request pipeline.
+            app.UseCors(options =>
+                options.WithOrigins(
+                    "http://172.20.10.7:4200",
+                    "http://localhost:4200")
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            return app;
+        }
+        public static IServiceCollection AddAppConfig(
+            this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.Configure<AppSettings>(config.GetSection("AppSettings"));
+            return services;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using AuthECAPI.Models;
+using AuthECAPI.Extensions;
+using AuthECAPI.Controllers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://10.8.0.155:5084");
+
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddSwaggerExplorer()
+                .InjectDbContext(builder.Configuration)  // Artık SQLite kullanıyor
+                .AddAppConfig(builder.Configuration)
+                .AddIdentityHandlersAndStores()
+                .ConfigureIdentityOptions()
+                .AddIdentityAuth(builder.Configuration);
+
+var app = builder.Build();
+
+app.ConfigureSwaggerExplorer()
+   .ConfigureCORS(builder.Configuration)
+   .AddIdentityAuthMiddlewares();
+
+app.MapControllers();
+app.MapGroup("/api")
+    .MapIdentityApi<AppUser>();
+app.MapGroup("/api")
+    .MapIdentitiyUserEndpoints()
+    .MapAccountEndpoints()
+    .MapAuthorizationDemoEndpoints();  
+
+app.Run();
